244e42d744551af06ae66bb6755da65cc30d8cdf
Jiradet Amornpimonkul <jiradet.amorn@gmail.com>
2022-03-21 22:45:04 -0400

    add location

--- MyWeather/View/CurrentTempCell.swift
+++ MyWeather/View/CurrentTempCell.swift
@@ -14,38 +14,43 @@ class CurrentTempCell: UITableViewCell {
     @IBOutlet weak var currentLocationLabel: UILabel!
     @IBOutlet weak var currentTempLabel: UILabel!
     @IBOutlet weak var currentConditionLabel: UILabel!
-    @IBOutlet weak var currentFeellikeLabel: UILabel!
+    @IBOutlet weak var currentMinMaxLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        
+        setupUI()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-        setupUI()
     }
     
     override func prepareForReuse() {
         currentLocationLabel.text = ""
         currentTempLabel.text = ""
+        currentConditionLabel.text = ""
+        currentMinMaxLabel.text = ""
     }
     
     private func setupUI() {
+        currentLocationLabel.text = ""
+        currentTempLabel.text = ""
+        currentConditionLabel.text = ""
+        currentMinMaxLabel.text = ""
         currentLocationLabel.font = UIFont(name: "Helvetica Neue", size: 24)
         currentTempLabel.font = UIFont(name: "Helvetica Neue", size: 62)
         currentConditionLabel.font = UIFont(name: "Helvetica Neue", size: 18)
-        currentFeellikeLabel.font = UIFont(name: "Helvetica Neue", size: 18)
+        currentMinMaxLabel.font = UIFont(name: "Helvetica Neue", size: 18)
     }
     
     func configure(data: CurrentTempModel?) {
-        guard let data = data else { return }
+        guard let data = data, let forecastday = data.forecast?.forecastday, let day = forecastday.first?.day else { return }
         
         currentLocationLabel.text = data.location?.name
         currentTempLabel.text = data.current?.string_temp_c
         currentConditionLabel.text = data.current?.condition?.text
-        currentFeellikeLabel.text = data.current?.string_feelslike_c
+        currentMinMaxLabel.text = " H:\(day.string_maxtemp_c) L:\(day.string_mintemp_c)"
     }
     
 }
